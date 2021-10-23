7b5d54178d3755e3f2f5288c0ab3a129dd5ce4bf
Diego <diegoulloao@icloud.com>
2021-10-23 14:40:04 -0300

    Initial commit

---
+++ svelte.config.js
@@ -0,0 +1,30 @@
+import preprocess from 'svelte-preprocess';
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+
+		vite: {
+			css: {
+				preprocessorOptions: {
+					scss: {
+						additionalData: '@import "src/variables.scss";'
+					}
+				}
+			}
+		}
+	},
+
+	preprocess: [
+		preprocess({
+			postcss: true,
+
+			scss: {
+				prependData: '@import "src/variables.scss";'
+			}
+		})
+	]
+};
+
+export default config;
