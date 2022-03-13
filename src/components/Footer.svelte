6be6e31dec1418c6f18172d17c609c4eb3075c09
Diego <diegoulloao@icloud.com>
2022-03-13 19:38:47 -0300

    fix: footer text color

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,4 +1,4 @@
-<section class="footer text-gray-800 text-center mt-32 mb-4">
+<section class="footer text-gray-700 text-center mt-32 mb-4">
   Copyright&copy; {new Date().getFullYear()}
   <br />
   <span class="text-xs">Made with Svelte</span>
