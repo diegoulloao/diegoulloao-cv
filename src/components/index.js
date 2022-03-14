f290d6d6de6e84b6ada4b99c948e1ae5c8c53925
Diego <diegoulloao@icloud.com>
2022-03-13 21:11:30 -0300

    fix: component not found

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 // Components
 import Nav from "@components/Nav.svelte"
-import Skill from "@components/SKill.svelte"
+import Skill from "@components/Skill.svelte"
 import Project from "@components/Project.svelte"
 import Experience from "@components/Experience.svelte"
 import Footer from "@components/Footer.svelte"
