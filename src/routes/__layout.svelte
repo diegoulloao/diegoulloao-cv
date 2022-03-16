47f75308ebb8291ba9f4118275268e392089fc6b
Diego <diegoulloao@icloud.com>
2022-03-16 00:36:30 -0300

    fix: layout responsive

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,7 +7,10 @@
 </script>
 
 <!-- Container -->
-<div class="container relative z-30 min-h-screen mx-auto py-8 flex flex-col">
+<div class={[
+  "container relative z-30 min-h-screen mx-auto py-8 flex flex-col",
+  "px-2 sm:px-3 md:px-4 lg:px-6 xl:px-0"
+].join(" ")}>
   <!-- Nav Menu -->
   <Nav />
 
