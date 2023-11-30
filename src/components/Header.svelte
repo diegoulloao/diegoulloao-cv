5f3e7c682e1b2beff1de6f163d1cd1246e7dba39
Diego <diegoulloao@icloud.com>
2023-11-30 02:11:43 -0300

    fix: texts, new cv 2024

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,7 +3,7 @@
   <nav class="nav-list mx-auto sm:mx-0 space-x-12 hover:text-blue">
     <a href="/">Home</a>
     <a href="/contact">Contact</a>
-    <a class="text-purple no-hover border-b-2 border-purple pb-1" href="/cv-diegoulloa.pdf" download>Get Resume</a>
+    <a class="text-purple no-hover border-b-2 border-purple pb-1" href="/diego-ulloa-resume-2024.pdf" download>Get Resume</a>
   </nav>
 
   <div class="social space-x-12 text-gray-500 hover:text-white">
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,17 @@
 <script>
-  // Components
-  import { Skill, Project, Experience, Button } from "@components"
+	// Components
+	import { Skill, Project, Experience, Button } from "@components";
 
-  // Data sources
-  import skills from "@data/skills.json"
-  import projects from "@data/projects.json"
-  import experience from "@data/experience.json"
+	// Data sources
+	import skills from "@data/skills.json";
+	import projects from "@data/projects.json";
+	import experience from "@data/experience.json";
 
-  // Seo
-  import Seo from "@seo"
+	// Seo
+	import Seo from "@seo";
 
-  // Years of experience
-  const yearsExperience = Math.abs(2011 - new Date().getFullYear()) - 2
+	// Years of experience
+	const yearsExperience = Math.abs(2011 - new Date().getFullYear()) - 2;
 </script>
 
 <!-- Seo -->
@@ -19,95 +19,103 @@
 
 <!-- Index Page -->
 <main id="home">
-  <section class="intro flex flex-col sm:flex-row mt-12 sm:mt-24 gap-10 sm:gap-14">
-    <div class="profile-img">
-      <img src="/diego.jpg" alt="none" class="rounded-full">
-    </div>
+	<section class="intro flex flex-col sm:flex-row mt-12 sm:mt-24 gap-10 sm:gap-14">
+		<div class="profile-img">
+			<img src="/diego.jpg" alt="none" class="rounded-full" />
+		</div>
 
-    <div class="description max-w-2xl flex flex-col justify-center text-gray-200">
-      <div>
-        <h2 class="text-4xl md:text-5xl mb-3 bg-title">&nbsp;Diego Ulloa&nbsp;</h2>
-      </div>
+		<div class="description max-w-2xl flex flex-col justify-center text-gray-200">
+			<div>
+				<h2 class="text-4xl md:text-5xl mb-3 bg-title">&nbsp;Diego Ulloa&nbsp;</h2>
+			</div>
 
-      <p class="text-gray-600">
-        Fullstack Developer, Open Source contributor and Self-taught Programmer since the age of 16.
-        <br /><br />
-        With {yearsExperience} years of net experience accumulated today I dedicate myself to develop Software mainly focused on Frontend.
-      </p>
+			<p class="text-gray-600">
+				Experienced Fullstack Developer with a passion for Open Source contributions. Self-taught
+				coder since age 16.
+			</p>
 
       <br />
 
-      <p>Currently working from Chile to Overseas.</p>
-    </div>
-  </section>
+			<p class="text-gray-600">
+				Bringing {yearsExperience} years of experience, I'm dedicated to helping companies build their products,
+				with a primary focus on Frontend development.
+			</p>
 
-  <section class="skills flex justify-end mt-16 sm:mt-10">
-    <div class="row-2 max-w-2xl text-right text-gray-600">
-      <h3 class="text-3xl mb-3 uppercase text-gray-200 bg-title">&nbsp;Skills&nbsp;</h3>
+			<br />
 
-      <div class="grid sm:flex grid-cols-2 skills gap-x-16 gap-y-8 sm:gap-16">
-        {#each skills as s}
-          <Skill skill={s} />
-        {/each}
-      </div>
-    </div>
-  </section>
+			<p>Currently working remotely for overseas companies.</p>
+		</div>
+	</section>
 
-  <section class="projects flex mt-16 sm:mt-10">
-    <div class="project-container w-full">
-      <h2 class="text-3xl uppercase bg-title mb-4">&nbsp;Contributions&nbsp;</h2>
+	<section class="skills flex justify-end mt-16 sm:mt-10">
+		<div class="row-2 max-w-2xl text-right text-gray-600">
+			<h3 class="text-3xl mb-3 uppercase text-gray-200 bg-title">&nbsp;Skills&nbsp;</h3>
 
-      <div class="projects-list grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3">
-        {#each projects as p}
-          <Project
-            repo={p.repo}
-            thumbnail={p.thumbnail}
-            description={p.description}
-            contributor={p.contributor}
-            labels={p.labels}
-            url={p.url}
-            classes="h-full"
-            />
-        {/each}
-      </div>
-    </div>
-  </section>
+			<div class="grid sm:flex grid-cols-2 skills gap-x-16 gap-y-8 sm:gap-16">
+				{#each skills as s}
+					<Skill skill={s} />
+				{/each}
+			</div>
+		</div>
+	</section>
 
-  <section class="experience mt-16 sm:mt-5 text-right">
-    <h2 class="bg-title text-3xl uppercase">&nbsp;Experience&nbsp;</h2>
+	<section class="projects flex mt-16 sm:mt-10">
+		<div class="project-container w-full">
+			<h2 class="text-3xl uppercase bg-title mb-4">&nbsp;Contributions&nbsp;</h2>
 
-    <div class="experience list">
-      {#each experience as e}
-        <Experience
-          company={e.company}
-          logo={e.logo}
-          period={e.period}
-          position={e.position}
-          location={e.location}
-          description={e.description}
-          classes="mt-4"
-        />
-      {/each}
-    </div>
-  </section>
+			<div
+				class="projects-list grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3"
+			>
+				{#each projects as p}
+					<Project
+						repo={p.repo}
+						thumbnail={p.thumbnail}
+						description={p.description}
+						contributor={p.contributor}
+						labels={p.labels}
+						url={p.url}
+						classes="h-full"
+					/>
+				{/each}
+			</div>
+		</div>
+	</section>
 
-  <div class="contact-me text-center mt-20 sm:mt-32">
-    <Button text="Contact me" href="/contact" />
-  </div>
+	<section class="experience mt-16 sm:mt-5 text-right">
+		<h2 class="bg-title text-3xl uppercase">&nbsp;Experience&nbsp;</h2>
+
+		<div class="experience list">
+			{#each experience as e}
+				<Experience
+					company={e.company}
+					logo={e.logo}
+					period={e.period}
+					position={e.position}
+					location={e.location}
+					description={e.description}
+					classes="mt-4"
+				/>
+			{/each}
+		</div>
+	</section>
+
+	<div class="contact-me text-center mt-20 sm:mt-32">
+		<Button text="Contact me" href="/contact" />
+	</div>
 </main>
 
 <style lang="scss">
-  .profile-img {
-    width: 250px;
+	.profile-img {
+		width: 250px;
 
-    @media only screen and (max-width: 1024px) {
-      width: auto;
-      max-width: 250px;
-    }
+		@media only screen and (max-width: 1024px) {
+			width: auto;
+			max-width: 250px;
+		}
 
-    @media only screen and (max-width: 640px) {
-      width: auto;
-      max-width: 230px !important;
-    }
-  }
+		@media only screen and (max-width: 640px) {
+			width: auto;
+			max-width: 230px !important;
+		}
+	}
 </style>
