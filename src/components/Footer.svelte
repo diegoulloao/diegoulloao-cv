99c6bc1bbb414e9f3cd8eb583eb237f2635cd430
Diego <diegoulloao@icloud.com>
2023-12-01 06:08:55 -0300

    fix: footer for mobile

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,5 @@
 <!-- Footer -->
-<footer class="footer text-gray-700 text-center mt-32 mb-4">
+<footer class="footer text-gray-700 text-center mt-24 sm:mt-32 mb-4">
   <span class="font-normal">Copyright&copy; {new Date().getFullYear()}</span>
   <br />
   <span class="text-xs">Made with SvelteKit</span>
