31e436ce6d90707da006a0dc5e97343055939af4
Diego <diegoulloao@icloud.com>
2023-12-01 07:21:20 -0300

    feat: scroll to top button

--- src/components/index.js
+++ src/components/index.js
@@ -1,20 +1,12 @@
 // Components
-import Header from "@components/Header.svelte"
-import Skill from "@components/Skill.svelte"
-import Project from "@components/Project.svelte"
-import Experience from "@components/Experience.svelte"
-import Footer from "@components/Footer.svelte"
-import Label from "@components/Label.svelte"
-import Button from "@components/Button.svelte"
-import ContactForm from "@components/ContactForm.svelte"
+import Header from "@components/Header.svelte";
+import Skill from "@components/Skill.svelte";
+import Project from "@components/Project.svelte";
+import Experience from "@components/Experience.svelte";
+import Footer from "@components/Footer.svelte";
+import Label from "@components/Label.svelte";
+import Button from "@components/Button.svelte";
+import ContactForm from "@components/ContactForm.svelte";
+import ScrollTop from "@components/ScrollTop.svelte";
 
-export {
-  Header,
-  Skill,
-  Project,
-  Experience,
-  Footer,
-  Label,
-  Button,
-  ContactForm
-}
+export { Header, Skill, Project, Experience, Footer, Label, Button, ContactForm, ScrollTop };
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,22 +1,27 @@
 <script>
-  // Components
-  import { Header, Footer } from "@components"
+	// Components
+	import { Header, Footer, ScrollTop } from "@components";
 
-  // Global Styles
-  import '@styles/app.scss'
+	// Global Styles
+	import "@styles/app.scss";
 </script>
 
 <!-- Container -->
-<div class={[
-  "container relative z-30 min-h-screen mx-auto py-8 flex flex-col",
-  "px-3 sm:px-3 md:px-4 lg:px-6 xl:px-0"
-].join(" ")}>
-  <!-- Nav Menu -->
-  <Header />
+<div
+	class={[
+		"container relative z-30 min-h-screen mx-auto py-8 flex flex-col",
+		"px-3 sm:px-3 md:px-4 lg:px-6 xl:px-0"
+	].join(" ")}
+>
+	<!-- Nav Menu -->
+	<Header />
 
-  <!-- Page Content -->
-  <slot />
+	<!-- Page Content -->
+	<slot />
 
-  <!-- Footer -->
-  <Footer />
+	<!-- Footer -->
+	<Footer />
+
+	<!-- Extra Elements -->
+	<ScrollTop />
 </div>
