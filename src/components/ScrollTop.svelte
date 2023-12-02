f71b0ab4f30bcb27f8b8d1348664c41f374a91c5
Diego <diegoulloao@icloud.com>
2023-12-02 05:13:43 -0300

    feat: scroll top button appears and disappears depending of the offset value

--- src/components/ScrollTop.svelte
+++ src/components/ScrollTop.svelte
@@ -1,18 +1,48 @@
 <script>
-	const onScrollTop = () => {
+	// Svelte
+	import { onMount } from "svelte";
+
+	// Constants
+	const OFFSET_SCROLL = 200;
+
+	// References
+	let scrollButtonRef;
+
+	const onScrollTopClick = () => {
 		window.scrollTo({ top: 0, behavior: "smooth" });
 	};
+
+	const onPageScroll = () => {
+		if (window.scrollY <= OFFSET_SCROLL) {
+      if (scrollButtonRef.classList.contains("opacity-0")) return
+
+			scrollButtonRef.classList.remove("opacity-1", "translate-y-0");
+			scrollButtonRef.classList.add("opacity-0", "translate-y-10");
+    } else {
+      if (!scrollButtonRef.classList.contains("opacity-0")) return
+
+			scrollButtonRef.classList.remove("opacity-0", "translate-y-10");
+      scrollButtonRef.classList.add("opacity-1", "translate-y-0")
+    }
+	};
+
+	onMount(() => {
+		window.addEventListener("scroll", onPageScroll);
+		return () => window.removeEventListener("scroll", onPageScroll);
+	});
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <div
+	bind:this={scrollButtonRef}
 	id="scroll-top-button"
 	class={[
 		"rounded-full border border-gray-800 w-11 h-11 sm:w-10 sm:h-10 bg-black select-none cursor-pointer",
 		"fixed bottom-0 right-0 mr-6 mb-6 sm:mr-4 sm:mb-4 flex justify-center items-center shadow-sm z-50",
-		"transition ease-in-out active:border-gray-200 group xl:hover:bg-gray-900 active:!bg-gray-200"
+		"transition ease-in-out active:border-gray-200 group xl:hover:bg-gray-900 active:!bg-gray-200",
+    "opacity-0 translate-y-10"
 	].join(" ")}
-	on:click={onScrollTop}
+	on:click={onScrollTopClick}
 >
 	<div
 		class="flex justify-center w-4 relative bottom-0 xl:group-hover:bottom-0.5 transition-all duration-200"
