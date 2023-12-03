c6e4f777ceae77d36342b95597d47b73d6e78051
Diego <diegoulloao@icloud.com>
2023-12-03 09:05:30 -0300

    Revert "feat: current page highlighted on menu"
    This reverts commit 03ac4f4b321ba522f3fbbc36dbc118ac968abbce.

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,32 +1,20 @@
 <script>
-	// Svelte
-	import { page } from "$app/stores";
-
-	// Data
-	import { rrss } from "@data/info.json";
+  // Data
+  import { rrss } from "@data/info.json"
 </script>
 
 <!-- Nav -->
-<header
-	class="sticky bg-black/75 backdrop-blur-2xl font-normal z-50 top-0 py-3 sm:px-8 flex justify-between shadow-md overflow-visible"
->
+<header class="sticky bg-black/75 backdrop-blur-2xl font-normal z-50 top-0 py-3 sm:px-8 flex justify-between shadow-md overflow-visible">
 	<nav class="flex nav-list mx-auto sm:mx-0 space-x-12 sm:space-x-14">
-		<a class="hover:text-[#3146c6]" class:text-[#3146c6]={$page.path === "/"} href="/">Home</a>
-		<a class="hover:text-[#3146c6]" class:text-[#3146c6]={$page.path === "/contact"} href="/contact"
-			>Contact</a
-		>
+		<a class="hover:text-[#3146c6]" href="/">Home</a>
+		<a class="hover:text-[#3146c6]" href="/contact">Contact</a>
 		<span class="flex">
 			<a
 				class="text-purple no-hover border-b-2 border-purple pb-1"
 				href="/diego-ulloa-resume-2024.pdf"
 				download><span class="hidden sm:inline">Get</span> Resume</a
 			>
-			<img
-				src="/icons/download.svg"
-				class="relative -top-0.5 ml-2"
-				width="16"
-				alt="download-icon"
-			/>
+			<img src="/icons/download.svg" class="relative -top-0.5 ml-2" width="16" alt="download-icon" />
 		</span>
 	</nav>
 
@@ -35,6 +23,8 @@
 
 		<span> | </span>
 
-		<a class="hover:text-white" href={rrss.linkedin} target="__blank"> Linkedin </a>
+		<a class="hover:text-white" href={rrss.linkedin} target="__blank">
+			Linkedin
+		</a>
 	</div>
 </header>
