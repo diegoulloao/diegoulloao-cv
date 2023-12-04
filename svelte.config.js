0b2c1127361cd40e737e0eece207e5ff833e6a44
Diego <diegoulloao@icloud.com>
2023-12-04 05:12:07 -0300

    feat: latest svelte/sveltekit package versions

--- svelte.config.js
+++ svelte.config.js
@@ -1,37 +1,11 @@
 import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-auto";
-import path from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
-
-		/* vite: {
-			css: {
-				preprocessorOptions: {
-					scss: {
-						additionalData: `
-              @import "src/styles/_variables.scss";
-              @import "src/styles/_colors.scss";
-            `
-					}
-				}
-			},
-
-			resolve: {
-				alias: {
-					"@": path.resolve("src"),
-					"@styles": path.resolve("src/styles"),
-					"@components": path.resolve("src/components"),
-					"@stores": path.resolve("src/stores"),
-					"@data": path.resolve("src/data"),
-					"@seo": path.resolve("src/seo")
-				}
-			}
-		} */
 	},
-
 	preprocess: [
 		preprocess({
 			postcss: true,
