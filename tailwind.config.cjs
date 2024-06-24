4b588ff1a8279b3ec4c1eaa609b80933aba64446
Diego <diegoulloao@icloud.com>
2024-06-24 17:20:38 -0400

    feat: packages upgrade

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,10 @@
 const config = {
-	mode: "jit",
-	content: ["./src/**/*.{html,js,svelte,ts}"],
-	theme: {
-		extend: {}
-	},
-	plugins: [require("tailwind-scrollbar")({ nocompatible: true })]
+  mode: "jit",
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {}
+  },
+  plugins: [require("tailwind-scrollbar")({ nocompatible: true })]
 };
 
 module.exports = config;
